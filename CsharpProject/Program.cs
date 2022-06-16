fbd40894b15d4f2954d01fbe5c5204f17e257503
DomPortello <doms.portello@gmail.com>
2022-06-16 17:03:11 +0200

    startOfProgram

--- CsharpProject/Program.cs
+++ CsharpProject/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CsharpProject;
+
+Console.WriteLine("Bienvenue sur le quizz de votre vie !");
+
+Console.WriteLine();
+
+Console.WriteLine("Merci de saisir votre nom");
+string? name = Console.ReadLine();
+Console.WriteLine("Merci de saisir votre prénom");
+string? lastName = Console.ReadLine();
+
+StringReader sr = new StringReader();
+
+//try
+//{
+//    createNewUser(name, lastName);
+//}
+//catch (Exception e)
+//{
+//    Console.WriteLine(e.Message);
+//}
+
+//Quizz quizz = new Quizz();
+//Quizz.Start(4);
+
+//while (quizz.nbOfQuestions <= quizz.maxQuestions)
+//{
+//    AskQuestion;
+//}
+
+
+//CheckUser(identity);
